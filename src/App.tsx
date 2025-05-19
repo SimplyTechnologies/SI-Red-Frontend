eac053230f4bf9aabeda820611c9696863109618
Annie-git-dev <annie.a.davtyan@gmail.com>
2025-05-19 10:01:40 +0400

    adding shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input";
 
 function App() {
   return (
     <>
       <div className="text-lg text-blue-600">Hello</div>
+      <Button>Button</Button>
+      <Button variant={"outline"}>Button</Button>
+      <Input />
     </>
   );
 }
