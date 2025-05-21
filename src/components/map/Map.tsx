3d8603044388a6d6816d68f324f180fdb453d7a8
Kehyan Levon <mrkheyan@gmail.com>
2025-05-21 23:30:58 +0400

    added experemental map

---
+++ src/components/map/Map.tsx
@@ -0,0 +1,35 @@
+import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+
+const center = { lat: 40.7128, lng: -74.006 };
+
+const carPositions = [
+  { lat: 40.7128, lng: -74.006 },
+  { lat: 40.715, lng: -74.002 },
+  { lat: 40.718, lng: -74.01 },
+  { lat: 40.71, lng: -74.015 },
+  { lat: 40.716, lng: -74.008 },
+];
+
+export default function CustomMap() {
+  return (
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY!}>
+      <Map
+        defaultCenter={center}
+        defaultZoom={13}
+        mapId="DEMO_MAP_ID"
+        style={{ width: "100%", height: "500px" }}
+      >
+        {carPositions.map((pos, index) => (
+          <AdvancedMarker key={index} position={pos}>
+            <img
+              src="/icons/mapCarMark.svg"
+              alt={`Car ${index + 1}`}
+              width={40}
+              height={40}
+            />
+          </AdvancedMarker>
+        ))}
+      </Map>
+    </APIProvider>
+  );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,9 @@
+import Map from "@/components/map/Map";
+
 export default function Dashboard() {
-  return <div className="text-xl font-bold">Dashboard</div>;
+  return (
+    <div className="text-xl font-bold">
+      <Map />
+    </div>
+  );
 }
