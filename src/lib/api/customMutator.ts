3dd0dece8a057b4c5c6957bf6b0e44f7a9a97847
Kehyan Levon <mrkheyan@gmail.com>
2025-05-28 18:53:20 +0400

    customMutator removed console.log()

--- src/lib/api/customMutator.ts
+++ src/lib/api/customMutator.ts
@@ -1,19 +1,15 @@
 export const customMutator = <T>({ url, method, data }: any): Promise<T> => {
   const token = localStorage.getItem("accessToken");
-  console.log("Token from localStorage:", token); // Log the token
+  const baseUrl = import.meta.env.VITE_API_URL;
 
-  return fetch(`http://localhost:3000${url}`, {
+  return fetch(`${baseUrl}${url}`, {
     method,
     headers: {
       "Content-Type": "application/json",
-      ...(token && { Authorization: `Bearer ${token}` }), // Log the headers
+      ...(token && { Authorization: `Bearer ${token}` }),
     },
     body: data ? JSON.stringify(data) : undefined,
   }).then((res) => {
-    console.log("Request Headers:", {
-      "Content-Type": "application/json",
-      ...(token && { Authorization: `Bearer ${token}` }),
-    });
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
