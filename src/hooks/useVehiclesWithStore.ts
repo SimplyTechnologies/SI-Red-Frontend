0626f1ef09aa62e4cb79086546958199a00c75f4
Annie-git-dev <annie.a.davtyan@gmail.com>
2025-05-29 02:45:10 +0400

    adding params in customMutator

--- src/hooks/useVehiclesWithStore.ts
+++ src/hooks/useVehiclesWithStore.ts
@@ -3,9 +3,7 @@ import { useGetVehicles } from '@/api/vehicle/vehicle';
 import { useVehiclesStore } from '@/store/useVehiclesStore';
 
 export const useVehiclesWithStore = () => {
-    const { data, isLoading, isError } = useGetVehicles({
-        userId: "4f1cedb7-a6b5-492d-9929-616ae9598d21",//TODO
-    });
+    const { data, isLoading, isError } = useGetVehicles();
     const setVehicles = useVehiclesStore((s) => s.setVehicles);
 
     useEffect(() => {
--- src/lib/api/customMutator.ts
+++ src/lib/api/customMutator.ts
@@ -1,14 +1,36 @@
-export const customMutator = <T>({ url, method, data }: any): Promise<T> => {
+export const customMutator = <T>({
+  url,
+  method,
+  data,
+  params,
+  signal,
+}: {
+  url: string;
+  method: string;
+  data?: any;
+  params?: Record<string, any>;
+  signal?: AbortSignal;
+}): Promise<T> => {
   const token = localStorage.getItem("accessToken");
   const baseUrl = import.meta.env.VITE_API_URL;
+  const queryString = params
+    ? "?" +
+      new URLSearchParams(
+        Object.entries(params).reduce((acc, [key, val]) => {
+          acc[key] = String(val);
+          return acc;
+        }, {} as Record<string, string>)
+      ).toString()
+    : "";
 
-  return fetch(`${baseUrl}${url}`, {
+  return fetch(`${baseUrl}${url}${queryString}`, {
     method,
     headers: {
       "Content-Type": "application/json",
       ...(token && { Authorization: `Bearer ${token}` }),
     },
     body: data ? JSON.stringify(data) : undefined,
+    signal,
   }).then((res) => {
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
