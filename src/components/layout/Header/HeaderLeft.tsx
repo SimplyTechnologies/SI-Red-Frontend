dd390ef51895d3dc2093201cbd0b6671e150ac1a
Kehyan Levon <mrkheyan@gmail.com>
2025-05-29 16:34:28 +0400

    assign to customer modal before refactoring and separating component

--- src/components/layout/Header/HeaderLeft.tsx
+++ src/components/layout/Header/HeaderLeft.tsx
@@ -3,6 +3,9 @@ import { useLocation } from "react-router-dom";
 import { useSidebarStore } from "@/store/useSidebarStore";
 import MenuLineHorizontal from "@/assets/icons/menu-line-horizontal.svg?react";
 
+import AssignToCustomerDialog from "@/components/assignToCustomer/AssignToCustomer";
+import { useState } from "react";
+
 const pageTitles: Record<string, string> = {
   "/": "Dashboard",
   "/vehicles": "Vehicles",
@@ -15,6 +18,8 @@ export default function HeaderLeft() {
   const location = useLocation();
   const { toggleMobileSidebar } = useSidebarStore();
   const pageTitle = pageTitles[location.pathname] || "Page";
+
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="flex items-center gap-4">
@@ -30,6 +35,16 @@ export default function HeaderLeft() {
       <h1 className="text-lg font-semibold text-sidebar-collapsed">
         {pageTitle}
       </h1>
+
+      {/* TODO: move to assign to customer when details will be ready. Temprory modal button */}
+      <button
+        onClick={() => setOpen(true)}
+        className="px-3 py-1 rounded-md bg-indigo-600 text-white text-sm"
+      >
+        Assign
+      </button>
+
+      <AssignToCustomerDialog open={open} onOpenChange={setOpen} />
     </div>
   );
 }
