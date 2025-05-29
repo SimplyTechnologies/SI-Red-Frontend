acb1f4618d19966768cafd7f11a16c196d91be08
Kehyan Levon <mrkheyan@gmail.com>
2025-05-29 10:59:14 +0400

    customers empty page

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/700.css";
 
-createRoot(document.getElementById('root')!).render(
-    <QueryClientProvider client={new QueryClient}>
-        <StrictMode>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </StrictMode>
-    </QueryClientProvider>
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={new QueryClient()}>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </QueryClientProvider>
 );
