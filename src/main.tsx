37fc2bf75497d04fceecb0682b38fa72c6c4466e
Kehyan Levon <mrkheyan@gmail.com>
2025-05-30 10:39:03 +0400

    removed unnecessary code

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LoadScript } from "@react-google-maps/api";
-import { Toaster } from "sonner";
 import App from "./App";
 import "./index.css";
 
@@ -16,7 +15,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <BrowserRouter>
           <>
             <App />
-            <Toaster richColors position="top-right" />
           </>
         </BrowserRouter>
       </LoadScript>
