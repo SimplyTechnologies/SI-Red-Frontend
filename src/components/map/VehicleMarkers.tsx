78f9110d5ecf717dc87a49787920d29daddbf6d5
Kehyan Levon <mrkheyan@gmail.com>
2025-05-30 15:24:34 +0400

    pins is clckable now

--- src/components/map/VehicleMarkers.tsx
+++ src/components/map/VehicleMarkers.tsx
@@ -1,7 +1,10 @@
 import { AdvancedMarker } from "@vis.gl/react-google-maps";
+import { useNavigate } from "react-router-dom";
 import type { VehicleResponse } from "@/api/schemas";
 
 export function VehicleMarkers({ vehicles }: { vehicles: VehicleResponse[] }) {
+  const navigate = useNavigate();
+
   return (
     <>
       {vehicles.map((vehicle) => {
@@ -12,12 +15,17 @@ export function VehicleMarkers({ vehicles }: { vehicles: VehicleResponse[] }) {
 
         return (
           <AdvancedMarker key={vehicle.id} position={{ lat, lng }}>
-            <img
-              src="/icons/mapCarMark.svg"
-              alt={`Car ${vehicle.vin}`}
-              width={40}
-              height={40}
-            />
+            <div
+              onClick={() => navigate(`/vehicles/${vehicle.id}`)}
+              className="cursor-pointer"
+            >
+              <img
+                src="/icons/mapCarMark.svg"
+                alt={`Car ${vehicle.vin}`}
+                width={40}
+                height={40}
+              />
+            </div>
           </AdvancedMarker>
         );
       })}
