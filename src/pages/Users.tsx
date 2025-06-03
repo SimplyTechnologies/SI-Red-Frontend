db3dc881e1efc59cd2b775b2d76cb27230d4602d
Annie-git-dev <annie.a.davtyan@gmail.com>
2025-06-03 16:31:11 +0400

    use reusable form for add new user

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,22 +1,44 @@
 import EmptyState from "@/components/custom/EmptyState";
 import SearchBar from "@/components/custom/customers/SearchBar";
-import AddNewUserDialog from "@/components/custom/users/AddNewUserDialog";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import TableHeaderComponent from "@/components/custom/customers/TableHeader";
 import { useState } from "react";
 import EmptyCustomersTableIcon from "@/assets/icons/emptyUsersTable.svg?react";
 import { useUserStore } from "@/store/useUserStore";
+import UserFormDialog from "@/components/assignToCustomerCreateUserDialog/FormDialog";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import UserForm from "@/components/assignToCustomerCreateUserDialog/Form";
 
 export default function Users() {
   const [search, setSearch] = useState("");
-  const { users, setUsers } = useUserStore();
+  const { users, userFormOpen, setUserFormOpen } = useUserStore();
 
   return (
     <>
       <div className="flex flex-col h-full px-6">
         <div className="flex justify-between items-center">
           <SearchBar search={search} setSearch={setSearch} />
-          <AddNewUserDialog title={"Add New User"} />
+          <Button
+            onClick={() => setUserFormOpen(true)}
+            className="h-[40px] w-[143px] px-[18px] py-[10px] rounded-[8px] gap-[6px] my-4 mb-5 flex items-center"
+          >
+            <Plus size={16} />
+            Add New User
+          </Button>
+
+          <UserFormDialog
+            open={userFormOpen}
+            onOpenChange={setUserFormOpen}
+            title={"Add New User"}
+          >
+            <UserForm
+              onSubmit={() => {
+                setUserFormOpen(false);
+              }}
+              submitLabel="Save"
+            />
+          </UserFormDialog>
         </div>
 
         <div className="flex-1 overflow-hidden bg-white shadow rounded-[16px] flex flex-col">
@@ -24,27 +46,23 @@ export default function Users() {
             <Table>
               <TableHeaderComponent />
               <TableBody>
-                {users.length ? 
+                {users.length ? (
                   users.map((user) => (
-                  <TableRow>
-                    <TableCell>
-                      {user.userName}
-                    </TableCell>
-                    <TableCell>
-                      {user.email}
-                    </TableCell>
-                    <TableCell>
-                      {user.phoneNumber}
-                    </TableCell>
-                  </TableRow>
-                  )) :
+                    <TableRow key={user.id}>
+                      <TableCell>{user.userName}</TableCell>
+                      <TableCell>{user.email}</TableCell>
+                      <TableCell>{user.phoneNumber}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
                   <EmptyState
                     text={
                       "At the moment, there are no users listed. However, you have the option to manually add new users."
                     }
                     info={"users"}
                     icon={EmptyCustomersTableIcon}
-                  />}
+                  />
+                )}
               </TableBody>
             </Table>
           </div>
--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -15,12 +15,14 @@ interface UserState {
   userName: string;
   phoneNumber: string;
   status: string;
+  userFormOpen: boolean;
   setEmail: (email: string) => void;
   setPassword: (password: string) => void;
   setUserName: (userName: string) => void;
   setPhoneNumber: (phone: string) => void;
   setStatus: (status: string) => void;
   setUsers: (users: User[]) => void;
+  setUserFormOpen: (isOpen: boolean) => void,
   reset: () => void;
 }
 
@@ -53,12 +55,14 @@ export const useUserStore = create<UserState>((set) => ({
   userName: "",
   phoneNumber: "",
   status: "",
+  userFormOpen: false,
   setEmail: (email) => set({ email }),
   setPassword: (password) => set({ password }),
   setUserName: (userName) => set({ userName }),
   setPhoneNumber: (phoneNumber) => set({ phoneNumber }),
   setStatus: (status) => set({ status }),
   setUsers: (users) => set({ users }),
+  setUserFormOpen: (userFormOpen) => set({ userFormOpen }),
   reset: () =>
     set({
       email: "",
