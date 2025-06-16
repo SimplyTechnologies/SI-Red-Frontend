b59cceafacad2c968546d424b84631fa65f5f694
tigran008 <tigran.s9.y@tumo.org>
2025-06-16 14:36:28 +0400

    Fix vehicle detail bug, add vehicle detail skeleton

--- src/components/custom/vehicles/VehiclesDetails/VehicleInfo.tsx
+++ src/components/custom/vehicles/VehiclesDetails/VehicleInfo.tsx
@@ -1,8 +1,8 @@
 import { useVehiclesStore } from "@/store/useVehiclesStore";
 import { getVehicleStatusIcon } from "@/utils/vehicleHelpers";
 import { formatDate } from "date-fns";
-import VehiclesTabListSkeleton from "../vehiclesTab/VehiclesTabListSkeleton";
 import type { VehicleStatusKeys } from "@/constants/constants";
+import VehicleInfoSkeleton from "./VehicleInfoSkeleton";
 
 interface Props {
   isLoading: boolean;
@@ -24,45 +24,48 @@ export default function VehicleInfo({ isLoading }: Props) {
 
   return (
     <div className="py-5 flex gap-2 w-full">
-      <VehicleStatusIcon />
-
-      {isLoading ? (
-        <VehiclesTabListSkeleton />
+      {isLoading || !selectedVehicle?.model ? (
+        <VehicleInfoSkeleton />
       ) : (
-        <div className="flex w-[350px]">
-          <div className="text-[14px] ml-3 w-full">
-            <p className="text-heading font-bold">{selectedVehicle?.vin}</p>
-            <p className="text-text-muted">
-              {selectedVehicle?.model?.name} {selectedVehicle?.model?.make.name}{" "}
-              {selectedVehicle?.year}
-            </p>
-            <p className="text-text-muted">
-              Location:{" "}
-              <span className="text-heading font-medium">
-                {selectedVehicle?.city}, {selectedVehicle?.street}
-              </span>
-            </p>
-            <p className="text-text-muted">
-              Date Created:{" "}
-              <span className="text-heading font-medium">{dateCreated}</span>
-            </p>
-            {selectedVehicle?.customer && (
-              <>
-                <p className="text-text-muted">
-                  Assigned at:{" "}
-                  <span className="text-heading font-medium">{AssignedAt}</span>
-                </p>
-                <p className="text-text-muted">
-                  Assigned to:{" "}
-                  <span className="text-heading font-medium">
-                    {selectedVehicle?.customer?.firstName}{" "}
-                    {selectedVehicle?.customer?.lastName}
-                  </span>
-                </p>
-              </>
-            )}
+        <>
+          <VehicleStatusIcon />
+          <div className="flex w-[350px]">
+            <div className="text-[14px] ml-3 w-full">
+              <p className="text-heading font-bold">{selectedVehicle?.vin}</p>
+              <p className="text-text-muted">
+                {selectedVehicle?.model?.name}{" "}
+                {selectedVehicle?.model?.make.name} {selectedVehicle?.year}
+              </p>
+              <p className="text-text-muted">
+                Location:{" "}
+                <span className="text-heading font-medium">
+                  {selectedVehicle?.city}, {selectedVehicle?.street}
+                </span>
+              </p>
+              <p className="text-text-muted">
+                Date Created:{" "}
+                <span className="text-heading font-medium">{dateCreated}</span>
+              </p>
+              {selectedVehicle?.customer && (
+                <>
+                  <p className="text-text-muted">
+                    Assigned at:{" "}
+                    <span className="text-heading font-medium">
+                      {AssignedAt}
+                    </span>
+                  </p>
+                  <p className="text-text-muted">
+                    Assigned to:{" "}
+                    <span className="text-heading font-medium">
+                      {selectedVehicle?.customer?.firstName}{" "}
+                      {selectedVehicle?.customer?.lastName}
+                    </span>
+                  </p>
+                </>
+              )}
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
