8584997db978ad95ecf0a1816c624086a0b0357d
Annie-git-dev <annie.a.davtyan@gmail.com>
2025-06-20 11:35:04 +0400

    tooltip adding

--- src/components/custom/users/UsersTableData.tsx
+++ src/components/custom/users/UsersTableData.tsx
@@ -5,6 +5,7 @@ import Avatar from "@/assets/icons/avatar.svg?react";
 import ConfirmationDialog from "../ConfirmationDialog";
 import { useState } from "react";
 import { Trash2 } from "lucide-react";
+import Tooltip from "@/components/tooltip";
 
 type Props = {
   user: any;
@@ -36,19 +37,21 @@ export default function UsersTableData({ user, handleDelete }: Props) {
         </TableCell>
         <TableCell>
           <div className="flex justify-center items-center w-full cursor-pointer">
-            <ConfirmationDialog
-              onConfirm={handleDelete}
-              title={`Delete ${DELETE_TITLE.USER}`}
-              description="Are you sure that you would like to delete this user? This action cannot be undone."
-              open={isOpen}
-              onOpenChange={setIsOpen}
-              showTrigger={true}
-              iconClassName="text-[#FA5087] bg-[#FFE0EA]"
-              triggerContent={
-                <Trash2 className="text-text-muted opacity-50 hover:text-heading hover:opacity-100 transition duration-300 ease-in-out" />
-              }
-              onConfirmArgs={[user.id]}
-            />
+            <Tooltip label="Delete" side="bottom" variant="reversed">
+              <ConfirmationDialog
+                onConfirm={handleDelete}
+                title={`Delete ${DELETE_TITLE.USER}`}
+                description="Are you sure that you would like to delete this user? This action cannot be undone."
+                open={isOpen}
+                onOpenChange={setIsOpen}
+                showTrigger={true}
+                iconClassName="text-[#FA5087] bg-[#FFE0EA]"
+                triggerContent={
+                  <Trash2 className="text-text-muted opacity-50 hover:text-heading hover:opacity-100 transition duration-300 ease-in-out" />
+                }
+                onConfirmArgs={[user.id]}
+              />
+            </Tooltip>
           </div>
         </TableCell>
       </TableRow>
